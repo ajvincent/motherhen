d1817d0c595cbabf79d7bb2bc497d31613882abf
trickypr <trickypr@icloud.com>
2022-06-19 16:46:49 +1000

    🐛  Add patch to fix build errors

---
+++ patches.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Exit if an error occurs within the script
+set -e
+
+action=$1
+
+last_patch=`exec ls -1 ./patches | sed 's/-.*//g' | sort -n | tail -1`
+next_patch=`expr 1 + $last_patch`
+root_pwd=$PWD
+
+if [ $action = "import" ]
+then
+    echo "Importing:"
+    echo
+    
+    cd ..
+    for file in $root_pwd/patches/*.patch
+    do
+        echo "  $file..."
+        patch -p1 < $file
+    done
+
+    cd $root_pwd
+elif [ $action = "export" ]
+then
+    if [ -x "$2" ]
+    then
+        echo "Please provide a file name. Usage: $0 $action <filename>"
+        exit 1
+    fi
+
+    echo "Exporting: ${@:2}"
+    echo
+    
+    cd ..
+    git add ${@:2}
+    git commit
+    git format-patch --minimal --start-number $next_patch -1 -o ./gecko-template/config/patches
+    cd $root_pwd
+else
+    echo "Usage: $0 import|export"
+    echo
+    echo "  import:  Import all patches in ./patches"
+    echo "  export:  Exports a specific patch. Usage: $0 export <filename>"
+fi
