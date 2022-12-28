3a6da71d249add3e42ebd11b78bc0dab2cc36b83
Alexander J. Vincent <ajvincent@gmail.com>
2022-12-28 14:18:28 -0800

    Remove create-application.py

--- create-application.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-print("Where is your clone of mozilla code you wish to append to?")
-mozillaCheckout = input("> ");
-# print(s);
-
-print("What directory path under this repository should I create an application for?")
-pathToProject = input("> ");
