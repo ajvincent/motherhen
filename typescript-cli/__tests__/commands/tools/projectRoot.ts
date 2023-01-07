9f6b1f90465de3c1e68443ea9a666b97e9823fde
Alexander J. Vincent <ajvincent@gmail.com>
2023-01-06 18:07:35 -0800

    Add --watch to jest options.

--- typescript-cli/__tests__/commands/tools/projectRoot.ts
+++ typescript-cli/__tests__/commands/tools/projectRoot.ts
@@ -1,7 +1,12 @@
 import path from "path";
 import url from "url";
 import projectRoot from "../../../commands/tools/projectRoot";
+
 it("projectRoot points to the root of this project", () => {
-  const actualPath = path.join(projectRoot, "typescript-cli/__tests__/commands/tools/projectRoot.ts");
+  const actualPath = path.join(
+    projectRoot,
+    "typescript-cli/__tests__/commands/tools/projectRoot.ts"
+  );
+
   expect(actualPath).toBe(url.fileURLToPath(import.meta.url));
 });
