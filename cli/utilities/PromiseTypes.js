c3fc09208a461cd3d7cae66974ba2717b518a50b
Alexander J. Vincent <ajvincent@gmail.com>
2023-01-11 12:00:24 -0800

    Add a PromiseQueue for filesystem operations.

--- cli/utilities/PromiseTypes.js
+++ cli/utilities/PromiseTypes.js
@@ -8,6 +8,30 @@
  * @see Array.prototype.map
  */
 export async function PromiseAllParallel(elementArray, callback) {
-    return Promise.all(elementArray.map(element => callback(element)));
+    return await Promise.all(elementArray.map(element => callback(element)));
+}
+export class PromiseQueue {
+    #tasks = [];
+    #started = false;
+    /**
+     * Append a task to the queue.
+     * @param task - the pending task to add.
+     */
+    appendTask(task) {
+        this.#assertNotStarted();
+        this.#tasks.push(task);
+    }
+    /** Run all tasks in the queue. */
+    async start() {
+        this.#assertNotStarted();
+        this.#started = true;
+        while (this.#tasks.length) {
+            await this.#tasks.shift()();
+        }
+    }
+    #assertNotStarted() {
+        if (this.#started)
+            throw new Error("I have already started running tasks!");
+    }
 }
 //# sourceMappingURL=PromiseTypes.js.map
--- typescript-cli/utilities/PromiseTypes.ts
+++ typescript-cli/utilities/PromiseTypes.ts
@@ -12,5 +12,38 @@ export async function PromiseAllParallel<E, V>(
   callback: (value: E) => Promise<V>
 ) : Promise<V[]>
 {
-  return Promise.all(elementArray.map(element => callback(element)));
+  return await Promise.all(elementArray.map(element => callback(element)));
+}
+
+type PromiseQueueTask = () => Promise<void>;
+export class PromiseQueue
+{
+  #tasks: PromiseQueueTask[] = [];
+  #started = false;
+
+  /**
+   * Append a task to the queue.
+   * @param task - the pending task to add.
+   */
+  appendTask(task: PromiseQueueTask) : void
+  {
+    this.#assertNotStarted();
+    this.#tasks.push(task);
+  }
+
+  /** Run all tasks in the queue. */
+  async start() : Promise<void>
+  {
+    this.#assertNotStarted();
+    this.#started = true;
+    while (this.#tasks.length) {
+      await (this.#tasks.shift() as PromiseQueueTask)();
+    }
+  }
+
+  #assertNotStarted() : void
+  {
+    if (this.#started)
+      throw new Error("I have already started running tasks!");
+  }
 }
