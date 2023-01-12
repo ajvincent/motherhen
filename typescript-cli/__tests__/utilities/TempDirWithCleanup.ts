99d27da8b85d664166d3266866f73537b1388a25
Alexander J. Vincent <ajvincent@gmail.com>
2023-01-11 20:32:04 -0800

    TempDirWithCleanup

---
+++ typescript-cli/__tests__/utilities/TempDirWithCleanup.ts
@@ -0,0 +1,17 @@
+import fs from "fs/promises";
+import path from "path";
+
+import TempDirWithCleanup from "#cli/utilities/TempDirWithCleanup";
+
+it("TempDirWithCleanup creates a temporary directory, and on demand, cleans it up", async () => {
+  const { tempDir, cleanupTempDir } = await TempDirWithCleanup();
+  const pathTwo = path.join(tempDir, "one/two"),
+        pathThree = path.join(pathTwo, "three.txt");
+
+  await fs.mkdir(pathTwo, { recursive: true });
+  await fs.writeFile(pathThree, "hello world", { "encoding": "utf-8" });
+
+  await cleanupTempDir();
+
+  await expect(fs.stat(pathThree)).rejects.toThrow("");
+});
