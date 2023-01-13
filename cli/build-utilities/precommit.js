57b0266a627ea4f371e2c6d30f69f3a92f3f5815
Alexander J. Vincent <ajvincent@gmail.com>
2023-01-12 16:40:06 -0800

    Speed up precommit checks.

---
+++ cli/build-utilities/precommit.js
@@ -0,0 +1,38 @@
+import { fork } from "child_process";
+import projectRoot from "../utilities/projectRoot.js";
+const promises = [
+    runESLint(),
+    runJest(),
+    checkTimestamps(),
+];
+await Promise.allSettled(promises);
+await Promise.all(promises);
+function runESLint() {
+    return forkProcess([], "node_modules/eslint/bin/eslint.js", ["--max-warnings=0", "typescript-cli"]);
+}
+function runJest() {
+    // node --experimental-vm-modules --no-warnings node_modules/jest/bin/jest.js --no-cache --all
+    return forkProcess(["--experimental-vm-modules", "--no-warnings"], "node_modules/jest/bin/jest.js", ["--no-cache", "--all", "--no-watchman"]);
+}
+function checkTimestamps() {
+    return forkProcess([], "cli/build-utilities/checkTimestamps.js", []);
+}
+function forkProcess(execArgv, pathToExec, args) {
+    const p = new Promise((resolve, reject) => {
+        const child = fork(pathToExec, args, {
+            cwd: projectRoot,
+            execArgv,
+            stdio: "inherit",
+        });
+        child.on("exit", code => {
+            if (code) {
+                reject(`failed ${pathToExec} with code ${code}`);
+            }
+            else {
+                resolve(`passed ${pathToExec}`);
+            }
+        });
+    });
+    return p.then(console.log);
+}
+//# sourceMappingURL=precommit.js.map
---
+++ typescript-cli/build-utilities/precommit.ts
@@ -0,0 +1,65 @@
+import { fork } from "child_process";
+import projectRoot from "../utilities/projectRoot.js";
+
+
+const promises = [
+  runESLint(),
+  runJest(),
+  checkTimestamps(),
+];
+
+await Promise.allSettled(promises);
+await Promise.all(promises);
+
+function runESLint() : Promise<void>
+{
+  return forkProcess(
+    [],
+    "node_modules/eslint/bin/eslint.js",
+    [ "--max-warnings=0",  "typescript-cli"],
+  );
+}
+
+function runJest() : Promise<void>
+{
+  // node --experimental-vm-modules --no-warnings node_modules/jest/bin/jest.js --no-cache --all
+  return forkProcess(
+    ["--experimental-vm-modules", "--no-warnings"],
+    "node_modules/jest/bin/jest.js",
+    ["--no-cache", "--all", "--no-watchman"],
+  )
+}
+
+function checkTimestamps() : Promise<void>
+{
+  return forkProcess(
+    [],
+    "cli/build-utilities/checkTimestamps.js",
+    [],
+  );
+}
+
+function forkProcess(
+  execArgv: string[],
+  pathToExec: string,
+  args: string[],
+) : Promise<void>
+{
+  const p = new Promise((resolve, reject) => {
+    const child = fork(pathToExec, args, {
+      cwd: projectRoot,
+      execArgv,
+      stdio: "inherit",
+    });
+    child.on("exit", code => {
+      if (code) {
+        reject(`failed ${pathToExec} with code ${code}`);
+      }
+      else {
+        resolve(`passed ${pathToExec}`);
+      }
+    });
+  });
+
+  return p.then(console.log);
+}
