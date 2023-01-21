d13bd81eee68b3f35f3edb573dc7a0fc1a5a8d0b
Alexander J. Vincent <ajvincent@gmail.com>
2023-01-20 23:40:59 -0800

    Replace setup wizard with the multisource one.

--- cli/commands/setup.js
+++ cli/commands/setup.js
@@ -1,14 +1,7 @@
 // #region preamble
-import buildBlank from "./setupWizard/blankConfig.js";
-import confirmSettings from "./setupWizard/confirmSettings.js";
-import fillVanilla from "./setupWizard/fillVanilla.js";
-import fillIntegration from "./setupWizard/fillIntegration.js";
-import getEditableJSON from "./setupWizard/getEditableJSON.js";
-import getKeyNameAndConfig from "./setupWizard/getKeyNameAndConfig.js";
-import { InterruptedPrompt } from "./setupWizard/inquirer-registration.js";
-import { maybeUpdateGitIgnore, } from "./setupWizard/updateGitIgnore.js";
-import pickConfigLocation from "./setupWizard/pickConfigLocation.js";
-import writeConfiguration from "./setupWizard/writeConfiguration.js";
+import projectRoot from "#cli/utilities/projectRoot.js";
+import RealInquirer, { InterruptedPrompt } from "#cli/utilities/inquirer-registration.js";
+import Driver from "#cli/configuration/version-1.0/wizard/Driver.js";
 // #endregion preamble
 /**
  * This function drives the set-up of a Motherhen configuration file.
@@ -19,43 +12,18 @@ import writeConfiguration from "./setupWizard/writeConfiguration.js";
  * to Inquirer.
  */
 export default async function setupMotherhen() {
-    let writePromise = () => {
-        throw new Error("assertion failure: we shouldn't reach this");
+    console.log(`
+This wizard will walk you through the process of crafting a Motherhen configuration file.
+You may abort the process at any time by pressing the ESC key.
+    `.trim() + "\n");
+    const driverArgs = {
+        workingDirectory: projectRoot,
+        inquirer: RealInquirer,
+        suppressConsole: false,
+        motherhenWriteDirectory: projectRoot
     };
     try {
-        writeIntroduction();
-        // Where are we going to write the configuration file?
-        const configPathWithUncreated = await pickConfigLocation();
-        const { pathToFile } = configPathWithUncreated;
-        let { uncreatedDirs } = configPathWithUncreated;
-        // If there's an existing configuration, get it.  Otherwise, create it in memory.
-        const { exists, output } = await getEditableJSON(pathToFile);
-        // Get the actual configuration object, and a property name to assign.
-        let key, config;
-        if (exists) {
-            ({ key, config } = await getKeyNameAndConfig(output));
-        }
-        else {
-            console.log(`Your configuration file does not exist.  I will use "default" as the project key.`);
-            key = "default";
-            config = buildBlank();
-        }
-        output[key] = config;
-        // Fill out the configuration's fields.
-        uncreatedDirs = await fillVanilla(pathToFile, config.vanilla, uncreatedDirs);
-        const integrationResults = await fillIntegration(pathToFile, config.vanilla.path, config.integration, uncreatedDirs);
-        ({ uncreatedDirs } = integrationResults);
-        void (uncreatedDirs);
-        const fullPathToMozconfig = config.integration.mozconfig;
-        // What changes should we make to .gitignore?
-        const updateGitIgnore = !exists && await maybeUpdateGitIgnore(pathToFile);
-        // Does everything look right?
-        const proceed = await confirmSettings(pathToFile, output, key);
-        if (!proceed) {
-            throw InterruptedPrompt.EVENT_INTERRUPTED;
-        }
-        // Update the real file system.
-        writePromise = () => writeConfiguration(pathToFile, exists, output, key, updateGitIgnore, integrationResults.replaceHatchedEggName, fullPathToMozconfig);
+        await Driver.run(driverArgs);
     }
     catch (error) {
         if (error === InterruptedPrompt.EVENT_INTERRUPTED) {
@@ -64,12 +32,5 @@ export default async function setupMotherhen() {
         }
         throw error;
     }
-    await writePromise();
-}
-function writeIntroduction() {
-    console.log(`
-This wizard will walk you through the process of crafting a Motherhen configuration file.
-You may abort the process at any time by pressing the ESC key.
-`.trim() + "\n");
 }
 //# sourceMappingURL=setup.js.map
--- typescript-cli/commands/setup.ts
+++ typescript-cli/commands/setup.ts
@@ -1,23 +1,11 @@
 // #region preamble
 
-import buildBlank from "./setupWizard/blankConfig.js";
-import confirmSettings from "./setupWizard/confirmSettings.js";
-import fillVanilla from "./setupWizard/fillVanilla.js";
-import fillIntegration from "./setupWizard/fillIntegration.js";
-import getEditableJSON from "./setupWizard/getEditableJSON.js";
-import getKeyNameAndConfig from "./setupWizard/getKeyNameAndConfig.js";
-import {
-  InterruptedPrompt
-} from "./setupWizard/inquirer-registration.js";
-import {
-  maybeUpdateGitIgnore,
-} from "./setupWizard/updateGitIgnore.js";
-import pickConfigLocation from "./setupWizard/pickConfigLocation.js";
-import type {
-  WritableConfigurationType,
-} from "./setupWizard/shared-types.js";
+import projectRoot from "#cli/utilities/projectRoot.js";
+import RealInquirer, { InterruptedPrompt } from "#cli/utilities/inquirer-registration.js";
 
-import writeConfiguration from "./setupWizard/writeConfiguration.js";
+import Driver, {
+  type DriverArguments
+} from "#cli/configuration/version-1.0/wizard/Driver.js";
 
 // #endregion preamble
 
@@ -31,70 +19,22 @@ import writeConfiguration from "./setupWizard/writeConfiguration.js";
  */
 export default async function setupMotherhen() : Promise<void>
 {
-  let writePromise: () => Promise<void> = () => {
-    throw new Error("assertion failure: we shouldn't reach this")
+  console.log(
+    `
+This wizard will walk you through the process of crafting a Motherhen configuration file.
+You may abort the process at any time by pressing the ESC key.
+    `.trim() + "\n"
+  );
+
+  const driverArgs: DriverArguments = {
+    workingDirectory: projectRoot,
+    inquirer: RealInquirer,
+    suppressConsole: false,
+    motherhenWriteDirectory: projectRoot
   };
 
   try {
-    writeIntroduction();
-
-    // Where are we going to write the configuration file?
-    const configPathWithUncreated = await pickConfigLocation();
-    const { pathToFile }  = configPathWithUncreated;
-    let { uncreatedDirs } = configPathWithUncreated;
-
-    // If there's an existing configuration, get it.  Otherwise, create it in memory.
-    const {exists, output} = await getEditableJSON(pathToFile);
-
-    // Get the actual configuration object, and a property name to assign.
-    let key: string, config: WritableConfigurationType;
-    if (exists) {
-      ({key, config} = await getKeyNameAndConfig(output));
-    }
-    else {
-      console.log(`Your configuration file does not exist.  I will use "default" as the project key.`);
-      key = "default";
-      config = buildBlank();
-    }
-
-    output[key] = config;
-
-    // Fill out the configuration's fields.
-    uncreatedDirs = await fillVanilla(
-      pathToFile,
-      config.vanilla,
-      uncreatedDirs
-    );
-
-    const integrationResults = await fillIntegration(
-      pathToFile,
-      config.vanilla.path,
-      config.integration,
-      uncreatedDirs
-    );
-    ({ uncreatedDirs } = integrationResults);
-    void(uncreatedDirs);
-    const fullPathToMozconfig = config.integration.mozconfig;
-
-    // What changes should we make to .gitignore?
-    const updateGitIgnore = !exists && await maybeUpdateGitIgnore(pathToFile);
-
-    // Does everything look right?
-    const proceed = await confirmSettings(pathToFile, output, key);
-    if (!proceed) {
-      throw InterruptedPrompt.EVENT_INTERRUPTED;
-    }
-
-    // Update the real file system.
-    writePromise = () : Promise<void> => writeConfiguration(
-      pathToFile,
-      exists,
-      output,
-      key,
-      updateGitIgnore,
-      integrationResults.replaceHatchedEggName,
-      fullPathToMozconfig,
-    );
+    await Driver.run(driverArgs);
   }
   catch (error) {
     if (error === InterruptedPrompt.EVENT_INTERRUPTED) {
@@ -104,16 +44,4 @@ export default async function setupMotherhen() : Promise<void>
 
     throw error;
   }
-
-  await writePromise();
-}
-
-function writeIntroduction() : void
-{
-  console.log(
-`
-This wizard will walk you through the process of crafting a Motherhen configuration file.
-You may abort the process at any time by pressing the ESC key.
-`.trim() + "\n"
-  );
 }
